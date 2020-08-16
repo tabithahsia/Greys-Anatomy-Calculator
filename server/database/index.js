2bb3b9137bb379c27e9a80c938cf2dd60a56a084
Tabitha <tabithahsia@gmail.com>
2020-08-16 14:41:32 -0700

    test

--- server/database/index.js
+++ server/database/index.js
@@ -8,7 +8,8 @@ class Database {
     constructor() {
         this._pool = new Pool({
             connectionString: CONNECTION_STRING,
-            ssl: SSL
+            // ssl: SSL
+            ssl: { rejectUnauthorized: false }
         });
 
         this._pool.on('error', (err) => {
